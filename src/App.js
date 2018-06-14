017228e96a4e8b711042dc0289a790ab157d2679
Diane Leigh <leighd2008@gmail.com>
2018-06-14 12:52:26 -0400

    add fetch to get data from api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { SWfilms } from './SWfilms'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      SWfilms: SWfilms,
+      SWfilms: [],
     }
 
   }
 
-  // componentDidMount() {
-  //   fetch('SWapi url')
-  //     .then(response=> response.json())
-  //     .then(users => {this.setState({ SWfilms: SWfilms})});
-  // }
+  componentDidMount() {
+    fetch('https://swapi.co/api/films/')
+      .then(response=> response.json())
+      .then(films => this.setState({ SWfilms: films}));
+  }
 
   render() {
-    return(
-      <div className='tc'>
-        <h1>Star Wars The Ultimate Guide</h1>
-        <h2>Click to choose a movie to Search!</h2>
-        <CardList SWfilms={this.state.SWfilms}/>
-      </div>
-    )
+    if (this.state.SWfilms.length === 0){
+      return <h1>Loading</h1>
+    } else {
+      return(
+        <div className='tc'>
+          <h1>Star Wars The Ultimate Guide</h1>
+          <h2>Click to choose a movie to Search!</h2>
+          {console.log('app.js', this.state.SWfilms)}
+          <CardList SWfilms={this.state.SWfilms.results}/>
+        </div>
+      )
+    }
+
   }
 }
 
--- src/CardList.js
+++ src/CardList.js
@@ -4,24 +4,27 @@ import moment from 'moment';
 
 
 const CardList = ({ SWfilms }) => {
+  {console.log('CardList', SWfilms)}
+
   return (
     <div>
+      {/* <p>{SWfilms[0].title}</p> */}
       {
-        SWfilms[0].results.map((film, i) => {
+        SWfilms.map((film, i) => {
           return (
             <Card
-              key={SWfilms[0].results[i].episode_id}
-              title={SWfilms[0].results[i].title}
-              episode={SWfilms[0].results[i].episode_id}
-              release_date={moment(SWfilms[0].results[i].release_date).format('MM/DD/YYYY')}
-              director={SWfilms[0].results[i].director}
-              producer={SWfilms[0].results[i].producer}
-              opening_crawl={SWfilms[0].results[i].opening_crawl}
-              characters={SWfilms[0].results[i].characters}
-              planets={SWfilms[0].results[i].planets}
-              species={SWfilms[0].results[i].species}
-              starships={SWfilms[0].results[i].starships}
-              vehicles={SWfilms[0].results[i].vehicles}
+              key={SWfilms[i].episode_id}
+              title={SWfilms[i].title}
+              episode={SWfilms[i].episode_id}
+              release_date={moment(SWfilms[i].release_date).format('MM/DD/YYYY')}
+              director={SWfilms[i].director}
+              producer={SWfilms[i].producer}
+              opening_crawl={SWfilms[i].opening_crawl}
+              characters={SWfilms[i].characters}
+              planets={SWfilms[i].planets}
+              species={SWfilms[i].species}
+              starships={SWfilms[i].starships}
+              vehicles={SWfilms[i].vehicles}
 
               options={['characters', 'planets', 'species', 'starships', 'vehicles']}
             />
